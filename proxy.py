6c9bbc61ac13d1f935941caecc68fb30847eccc1
Andrew Pagan <andrew.r.pagan@gmail.com>
2017-03-14 16:05:53 -0700

    Bleh

--- proxy.py
+++ proxy.py
@@ -2,6 +2,7 @@
 # I really like networks, so that's why I'm doing this
 # And I like Python
 # . . . Yes
+# https://null-byte.wonderhowto.com/how-to/sploit-make-proxy-server-python-0161232/
 
 import socket, sys
 from thread import *
@@ -42,7 +43,35 @@ def start():
 			print "\n [*] Proxy Server Shutting Down ..."
 			print "[*] Have A Nice Day"
 			sys.exit()
-	
 	s.close()
+
+def conn_string(conn, data, addr):
+	try:
+		first_line = data.split('\n')[0]
+		
+		url = first_line.split(' ')[1]
+		
+		http_pos = url.find("://")
+		if (http_pos == 1):
+			temp = url
+		else:
+			temp = url[(http_pos+3):]
+		port_pos = temp.find(":")
+		
+		webserver_pos = temp.find("/")
+		if webserver_pos == -1:
+			webserver_pos = len(temp)
+		webserver = ""
+		port = -1
+		if (port_pos == -1 or webserver_pos < port_pos):
+			port = 80
+			webserver = temp[:webserver_pos]
+		else:
+			port = int((temp[(port_pos+1):])[:webserver_pos-port_pos-1])
+			webserver = temp[:port_pos]
+		
+		proxy_server(webserver, port, conn, addr, data)
+	except Exception, e:
+		pass
 
 start()
